9a393e08e58f8a747c3e352240c1b42e52aad96b
Ricky Nyairo <rickynyairo@gmail.com>
2018-04-24 22:23:50 +0300

    testing with live figures

--- ghapp/views.py
+++ ghapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from .models import SensorData, ActivityMeta
-#from .GHMCS_OO import GreenhouseSystem as GreenHouse
+from .GHMCS_OO import GreenhouseSystem as GreenHouse
 
 def index(request):
 	return render(request, 'ghapp/index.html')
