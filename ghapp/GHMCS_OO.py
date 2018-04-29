9f1793d6d9b43080b87270c752e91a4a2ad2c813
Ricky Nyairo <rickynyairo@gmail.com>
2018-04-29 16:23:06 +0300

    switching lights using a button, trial 1

--- ghapp/GHMCS_OO.py
+++ ghapp/GHMCS_OO.py
@@ -2,11 +2,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import Adafruit_DHT
-#from django.utils import timezone
-#from models import SensorData
-#Pin Setup
-#ldr == light dependant resistor
+
 ldr=24
+led = 13
 fan_forward = 20
 soil_moisture = 21
 water_pump = 16
@@ -17,6 +15,7 @@ dht_pin = 19
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan_forward, GPIO.OUT)
+GPIO.setup(led, GPIO.OUT)
 GPIO.setup(water_pump, GPIO.OUT)
 GPIO.setup(soil_moisture, GPIO.IN)
 GPIO.setup(vent_servo, GPIO.OUT)
@@ -33,6 +32,16 @@ class GreenhouseSystem(object):
 		else:
 			state='dry'
 		return state
+
+	def switch_lights(self, state='on'):
+		if state is 'on':
+			GPIO.output(led, GPIO.HIGH)
+		elif state is 'off':
+			GPIO.output(led, GPIO.HIGH)
+		else:
+			GPIO.output(led, GPIO.LOW)
+
+
 
 	def get_temperature(self):
 		humidity, temperature = Adafruit_DHT.read_retry(
