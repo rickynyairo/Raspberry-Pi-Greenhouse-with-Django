f15012c45b2bd3f64d7689a9b309a92c41ae0db9
Ricky Nyairo <rickynyairo@gmail.com>
2018-06-08 18:18:01 +0300

    static files

--- static/ghapp/js/control_panel.js
+++ static/ghapp/js/control_panel.js
@@ -3,13 +3,17 @@ var url = "http://raspi/ghapp/commands/";
 
 var lights_state = false;
 $("#lights_button").click(function(){
+	console.log("clicked");
     if (!lights_state){
+    	console.log("turning lights on");
     	$.post(url,{"command":101},function(data, status){
-    		alert(data);
+    		//alert(data);
+    		console.log(data);
     		$("#response_label").text(data);
     		lights_state = true;
     	});
     }else{
+    	console.log("turning lights off");
     	$.post(url,{"command":100},function(data, status){
     		alert(data);
     		$("#response_label").text(data);
