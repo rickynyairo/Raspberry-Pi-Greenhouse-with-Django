b1c2af017785baa66fd0e4c4e5ff4d9856fcef8f
Ricky Nyairo <rickynyairo@gmail.com>
2018-06-08 20:06:50 +0300

    static files

--- static/ghapp/js/control_panel.js
+++ static/ghapp/js/control_panel.js
@@ -9,15 +9,15 @@ $(document).ready(function(){
 	    if (!lights_state){
 	    	console.log("turning lights on");
 	    	$.post(url,{"command":101},function(data, status){
-	    		console.log(data);
-	    		$("#response_label").text(data);
+	    		console.log(data.text);
+	    		$("#response_label").text(data.text);
 	    		lights_state = true;
 	    	});
 	    }else{
 	    	console.log("turning lights off");
 	    	$.post(url,{"command":100},function(data, status){
-	    		alert(data);
-	    		$("#response_label").text(data);
+	    		console.log(data.text);
+	    		$("#response_label").text(data.text);
 	    		lights_state = false;
 	    	});
 	    }
