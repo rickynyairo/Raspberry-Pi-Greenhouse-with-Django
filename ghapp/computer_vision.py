bcb59816d31e994a064585fee0d0e30d4a2bafb1
Ricky Nyairo <rickynyairo@gmail.com>
2018-07-16 12:40:58 +0300

    deploying cv

--- ghapp/computer_vision.py
+++ ghapp/computer_vision.py
@@ -16,6 +16,10 @@ def detect_properties(image_path):
     max_red = properties.dominant_colors.colors[0].color.red
     fraction = 0#properties.dominant_colors.colors[0].pixel_fraction
     for color in properties.dominant_colors.colors:
+        print('fraction: {}'.format(color.pixel_fraction))
+        print('\tr: {}'.format(color.color.red))
+        print('\tg: {}'.format(color.color.green))
+        print('\tb: {}'.format(color.color.blue))
         if color.color.red > max_red:
             max_red = color.color.red
         if color.color.red >= 185:  
@@ -24,30 +28,7 @@ def detect_properties(image_path):
 
 def main():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/pi/Desktop/fyp/web_app/key/fyp-ghmcs-cd159d1de565.json"
-    sample_grass = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_grass_1.jpeg"
-        )
-    sample_grass_with_red_dot = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_grass_with_red_dot.jpg"
-        )
-    sample_grass_with_red_dots = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_grass_with_red_dots.jpg"
-        )
-    sample_weeds_5 = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_weeds_5.jpeg"
-        )
-    sample_weeds_6 = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_weeds_6.jpeg"
-        )
-    sample_leaves = os.path.join(
-        os.getcwd(), 
-        "static/ghapp/images/sample_leaves_1.jpeg"
-        )
+
     no_weeds = os.path.join(
         os.getcwd(), 
         "static/ghapp/images/no_weeds.jpeg"
@@ -65,22 +46,6 @@ def main():
         "static/ghapp/images/few_weeds.jpeg"
         )
 
-    def tester():
-        print ("No red dot:")
-        print (detect_properties(sample_grass))
-        print ("With red dot:")
-        print (detect_properties(sample_grass_with_red_dot))
-        print ("With red dots:")
-        print (detect_properties(sample_grass_with_red_dots))
-    
-    def tester2():
-        print ("Leaves")
-        print (detect_properties(sample_leaves))
-        print ("With a few weeds: ")
-        print (detect_properties(sample_weeds_6))
-        print ("With a more weeds: ")
-        print (detect_properties(sample_weeds_5))
-
     def actual():
         print ("No weeds")
         print (detect_properties(no_weeds))
@@ -91,8 +56,6 @@ def main():
         print ("With more weeds 2: ")
         print (detect_properties(lots_of_weeds_2))
 
-    #tester()
-    #tester2()
     actual()
 
 if __name__ == "__main__":
